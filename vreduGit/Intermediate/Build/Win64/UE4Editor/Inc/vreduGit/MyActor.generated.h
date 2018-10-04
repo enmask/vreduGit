d74a2a75084887e05935e9a0f1389504fcfd39f6
enmask <43369907+enmask@users.noreply.github.com>
2018-10-05 00:09:20 +0200

    Test 2
    Code test version 2

---
+++ vreduGit/Intermediate/Build/Win64/UE4Editor/Inc/vreduGit/MyActor.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VREDUGIT_MyActor_generated_h
+#error "MyActor.generated.h already included, missing '#pragma once' in MyActor.h"
+#endif
+#define VREDUGIT_MyActor_generated_h
+
+#define vreduGit_Source_vreduGit_MyActor_h_12_RPC_WRAPPERS
+#define vreduGit_Source_vreduGit_MyActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define vreduGit_Source_vreduGit_MyActor_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyActor(); \
+	friend VREDUGIT_API class UClass* Z_Construct_UClass_AMyActor(); \
+public: \
+	DECLARE_CLASS(AMyActor, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/vreduGit"), NO_API) \
+	DECLARE_SERIALIZER(AMyActor) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define vreduGit_Source_vreduGit_MyActor_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyActor(); \
+	friend VREDUGIT_API class UClass* Z_Construct_UClass_AMyActor(); \
+public: \
+	DECLARE_CLASS(AMyActor, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/vreduGit"), NO_API) \
+	DECLARE_SERIALIZER(AMyActor) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define vreduGit_Source_vreduGit_MyActor_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyActor(AMyActor&&); \
+	NO_API AMyActor(const AMyActor&); \
+public:
+
+
+#define vreduGit_Source_vreduGit_MyActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyActor(AMyActor&&); \
+	NO_API AMyActor(const AMyActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMyActor)
+
+
+#define vreduGit_Source_vreduGit_MyActor_h_12_PRIVATE_PROPERTY_OFFSET
+#define vreduGit_Source_vreduGit_MyActor_h_9_PROLOG
+#define vreduGit_Source_vreduGit_MyActor_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	vreduGit_Source_vreduGit_MyActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	vreduGit_Source_vreduGit_MyActor_h_12_RPC_WRAPPERS \
+	vreduGit_Source_vreduGit_MyActor_h_12_INCLASS \
+	vreduGit_Source_vreduGit_MyActor_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define vreduGit_Source_vreduGit_MyActor_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	vreduGit_Source_vreduGit_MyActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	vreduGit_Source_vreduGit_MyActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	vreduGit_Source_vreduGit_MyActor_h_12_INCLASS_NO_PURE_DECLS \
+	vreduGit_Source_vreduGit_MyActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID vreduGit_Source_vreduGit_MyActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
